aa152d411b892f5477f3b3664e43bdf005a08d8a
madifan <madifan@stanford.edu>
2025-05-20 18:51:19 -0700

    Created placeholder Feed.tsx

---
+++ WebsiteCS278/client/src/pages/Feed.tsx
@@ -0,0 +1,11 @@
+import { BottomToolbar } from "@/components/BottomToolbar";
+
+export default function Feed() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-6">
+      <h1 className="text-2xl font-bold mb-4">Feed</h1>
+      <p className="text-lg text-gray-500">This is a placeholder page for feed.</p>
+      <BottomToolbar />
+    </div>
+  );
+}
