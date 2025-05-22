cef56847ceddeef2c762294c2c9c46e5c6308080
cjonas9 <christianjonas99@gmail.com>
2025-05-22 14:35:10 -0700

    added CORS handling, updated reqs

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 import db_query
 import sql_query
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def index():
