1d8edc34942a168323052ec00b3089ac09899189
cjonas9 <christianjonas99@gmail.com>
2025-05-23 14:00:48 -0700

    modified api.py

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 import sqlite3
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 BROADCASTR_DB = "./data/broadcastr.db"
 print("Backend is running on port 8000...")
