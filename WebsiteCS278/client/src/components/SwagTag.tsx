4adb3df35f93124713e736dc7b3dd6ede9e87c75
madifan <madifan@stanford.edu>
2025-05-26 17:18:27 -0700

    Update SwagTag.tsx
    - standardized icon use

--- WebsiteCS278/client/src/components/SwagTag.tsx
+++ WebsiteCS278/client/src/components/SwagTag.tsx
@@ -2,16 +2,16 @@
 SwagTag.tsx: Component for the swag tag
 ---------------------------------------
 EXAMPLE USAGE:
-<SwagTag text="255" icon={<Star size={16} />} />
+<SwagTag text="255" />
 */
 
 import { LightningBoltIcon } from "@/lib/icons"
 
-export default function SwagTag ({text, icon}: {text: string, icon: React.ReactNode}) {
+export default function SwagTag ({text}: {text: string}) {
   return (
     <div className="inline-flex items-center gap-1 px-3 py-2 bg-[#654DC4] rounded-full">
       <span className="text-white font-medium">{text}</span>
-      {icon && <LightningBoltIcon className="h-5 w-5 text-yellow-300" />}
+      <LightningBoltIcon className="h-5 w-5 text-yellow-300" />
     </div>
   )
 }
