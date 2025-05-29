e4d956a3b1dbef5ac20f992265e2d4505aabcb9c
Lucas Scott <lmscott17@ymail.com>
2025-05-29 00:31:21 -0400

    Added API for getting song swaps

--- api_song_swap.py
+++ api_song_swap.py
@@ -161,3 +161,116 @@ def api_add_song_swap_reaction():
     connection.close()
 
     return jsonify({"success": True}), 200
+
+@song_swap_bp.route("/api/get-song-swaps")
+def api_get_song_swaps():
+    """
+    Retrieves song swaps.
+        If song swap id is specfied, just that song swap is returned
+            (else specify zero for all).
+        If a user is specified, all song swaps for that user are returned
+        If neither is specified, all song swaps are returned (based on limit)
+    Example:
+        GET /api/get-song-swaps?user=LastFmProfileName&songswapid=n&limit=n
+    Returns JSON:
+      {
+        "songSwaps": [
+          { "id": int, 
+            "initiated_user_id": int,
+            "initiated_user": str,
+            "matched_user_id": int,
+            "matched_user": str,
+            "initiated_track_id": int,
+            "initiated_track_name": str,
+            "initiated_artist_id": int,
+            "initiated_artist_name": str,
+            "matched_track_id": int,
+            "matched_track_name": str,
+            "matched_artist_id": int, 
+            "matched_artist_name": str,
+            "initiated_reaction": int,
+            "matched_reaction": int,
+            "swap_initiated_timestamp": str,
+            "initiated_track_timestamp": str,
+            "matched_track_timestamp": str,
+            "initiated_reaction_timestamp": str,
+            "matched_reaction_timestamp": str
+          },
+          …
+        ]
+      }
+    """
+    user = request.args.get("user", "")
+    song_swap_id = int(request.args.get("songswapid", "0"))
+    limit = int(request.args.get("limit", 50))
+
+    user_id = sql_query.query_user_id(user)
+
+    sql = """
+        SELECT DISTINCT SongSwap.SongSwapID AS id, 
+            InitiatedUser.UserID AS initiated_user_id, InitiatedUser.LastFmProfileName AS initiated_user,
+            MatchedUser.UserID AS matched_user_id, MatchedUser.LastFmProfileName AS matched_user,
+            InitiatedTrack.TrackID AS initiated_track_id, InitiatedTrack.TrackName AS initiated_track_name,
+            InitiatedArtist.ArtistID AS initiated_artist_id, InitiatedArtist.ArtistName AS initiated_artist_name,
+            MatchedTrack.TrackID AS matched_track_id, MatchedTrack.TrackName AS matched_track_name,
+            MatchedArtist.ArtistID AS matched_artist_id, MatchedArtist.ArtistName AS matched_artist_name,
+            SongSwap.InitiatedReaction AS initiated_reaction, SongSwap.MatchedReaction AS matched_reaction,
+            SongSwap.SwapInitiatedTimestamp AS swap_initiated_timestamp,
+            SongSwap.InitiatedTrackTimestamp AS initiated_track_timestamp,
+            SongSwap.MatchedTrackTimestamp AS matched_track_timestamp,
+            SongSwap.InitiatedReactionTimestamp AS initiated_reaction_timestamp,
+            SongSwap.MatchedReactionTimestamp AS matched_reaction_timestamp	   
+        FROM SongSwap
+        LEFT JOIN User AS InitiatedUser ON SongSwap.InitiatedUserID = InitiatedUser.UserID
+        LEFT JOIN User AS MatchedUser ON SongSwap.MatchedUserID = MatchedUser.UserID
+        LEFT JOIN Track AS InitiatedTrack ON SongSwap.InitiatedTrackID = InitiatedTrack.TrackID
+        LEFT JOIN Artist AS InitiatedArtist ON InitiatedTrack.ArtistID = InitiatedArtist.ArtistID
+        LEFT JOIN Track AS MatchedTrack ON SongSwap.MatchedTrackID = MatchedTrack.TrackID
+        LEFT JOIN Artist AS MatchedArtist ON MatchedTrack.ArtistID = MatchedArtist.ArtistID
+        WHERE 1=1
+    """
+
+    if song_swap_id != 0:
+        sql += f"   AND SongSwap.SongSwapID = {song_swap_id}"
+
+    if user_id != 0:
+        sql += f"   AND (InitiatedUser.UserID = {user_id} OR MatchedUser.UserID = {user_id})"
+
+    sql += """
+        ORDER BY SongSwap.SongSwapID DESC /* Newest to oldest */
+        LIMIT ?
+    """
+
+    # print(f"song swaps query: {sql}")
+
+    conn = sql_query.get_db_connection()
+    rows = conn.execute(sql, (limit,)).fetchall()
+    conn.close()
+
+    song_swaps = [
+        {
+            "id":                           row["id"],
+            "initiated_user_id":            row["initiated_user_id"],
+            "initiated_user":               row["initiated_user"],
+            "matched_user_id":              row["matched_user_id"],
+            "matched_user":                 row["matched_user"],
+            "initiated_track_id":           row["initiated_track_id"],
+            "initiated_track_name":         row["initiated_track_name"],
+            "initiated_artist_id":          row["initiated_artist_id"],
+            "initiated_artist_name":        row["initiated_artist_name"],
+            "matched_track_id":             row["matched_track_id"],
+            "matched_track_name":           row["matched_track_name"],
+            "matched_artist_id":            row["matched_artist_id"],
+            "matched_artist_name":          row["matched_artist_name"],
+            "initiated_reaction":           row["initiated_reaction"],
+            "matched_reaction":             row["matched_reaction"],
+            "swap_initiated_timestamp":     row["swap_initiated_timestamp"],
+            "initiated_track_timestamp":    row["initiated_track_timestamp"],
+            "matched_track_timestamp":      row["matched_track_timestamp"],
+            "initiated_reaction_timestamp": row["initiated_reaction_timestamp"],
+            "matched_reaction_timestamp":   row["matched_reaction_timestamp"]
+        }
+        for row in rows
+    ]
+
+    return jsonify({ "songSwaps": song_swaps })
